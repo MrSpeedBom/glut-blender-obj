89dcb3cf1fa4e6e0b140320a77d8ad7f50084abf
MrSpeedBom <ahmad380.mh@hotmail.com>
2023-11-14 14:04:51 +0200

    original project

---
+++ FunLandOpengl/mainMemory.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_MEMORY_H
+#define MAIN_MEMORY_H
+
+//put all includes here
+#include <windows.h>
+#include <GL/glut.h>
+
+#include "Timer.h"
+#include "Drawable.h"
+using namespace std;
+
+
+#endif 
